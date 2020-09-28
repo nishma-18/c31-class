84c6a00209a809f2c6fce64fb067b55fd97400fb
nishma-18 <69460964+nishma-18@users.noreply.github.com>
2020-09-28 16:39:28 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,26 @@
+class Bird extends BaseClass {
+  constructor(x,y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/bird.png");
+    this.projectileImage= loadImage("sprites/smoke.png");
+    this.projectile= [];
+  }
+
+  display() {
+    //this.body.position.x = mouseX;
+    //this.body.position.y = mouseY;
+   
+    super.display();
+
+    if(this.body.velocity.x>10 && this.body.position.x>200){
+      var position= [this.body.position.x, this.body.position.y];
+      this.projectile.push(position);
+    }
+
+    
+    for (var i=0; i<this.projectile.length; i++){
+    image(this.projectileImage, this.projectile[i][0], this.projectile[i][1]);
+    }
+
+  }
+}
